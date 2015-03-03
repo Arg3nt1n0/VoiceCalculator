f23bb6c0e841201a6a79423a7f27b4d32fca3c86
Jonathan Argumedo <Arg3nt1n0@users.noreply.github.com>
2015-03-02 19:52:29 -0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,41 @@
+function onDeviceReady(){
+                console.log("Device is ready");
+            }
+
+            function escuchar() {
+                var maxMatches = 5;
+                var promptString = "Speak now"; // optional
+                var language = "en-US";                     // optional
+                window.plugins.speechrecognizer.startRecognize(function(result){
+                    $("#resultado_calculadora").append(result);
+                }, function(errorMessage){
+                    $("#resultado_calculadora").append("Error message: " + errorMessage).text();
+                }, maxMatches, promptString, language);
+            }
+
+            document.addEventListener("deviceready", onDeviceReady, true);
+
+$(function()
+{
+	$("#calculadora tr td").click(function()
+	{
+		if ($(this).text() != "C" && $(this).text() != "=")
+		{
+			$("#resultado_calculadora").append($(this).text());
+		}
+		if ($(this).text() == "=")
+		{
+			operacion = $("#resultado_calculadora").text().split("");
+			total = "";
+			for ( var index in operacion)
+			{
+				total = total + operacion[index];
+			}
+			$("#resultado_calculadora").text(eval(total));
+		}
+		if($(this).text() == "C")
+		{
+			$("#resultado_calculadora").text("");
+		}
+	});
+});
