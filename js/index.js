ab828de595fc8bb82dfdffff5c1dde278b61f903
argumedo <gowpga@hotmail.com>
2015-03-04 10:27:34 -0700

    Commited Voice Repo

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
+var capturador;
+
 document.addEventListener("deviceready", function()
 {
 	capturador = navigator.device.capture;
 }, false);
 
-function CapturarAudio()
-{
-	capturador.captureAudio(capturaOK, capturaError);
+function audio(){
+	capturador.captureAudio(capturoOK, capturoError);
 }
 
-function capturaError()
-{
-	alert("Error al capturar");
-}
-
-function capturaOK(archivos)
-{
-	for(var i=0; i<archivos.length; i++)
-	{
+function capturoOK(archivos){
+	for(var i=0; i<archivos.length; i++){
 		var ruta = archivos[i].fullPath;
-		$("#resultado_calculadora").append(ruta.text());
+		$("#resultado_calculadora").append(ruta);
+		$("#resultado_calculadora").append(ruta).text();
 	}
+}
+
+function capturoError(){
+	alert("Error al capturar");
+		$("#resultado_calculadora").append("ERROR CAPTURANDO");
+		$("#resultado_calculadora").append("ERROR CAPTURANDO").text();
 }
